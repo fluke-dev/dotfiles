7ca4cffa56a1e90530ae81df9707a707a716c420
fluke-dev <bunbunfluke@gmail.com>
2024-11-16 18:48:33 +0700

    First commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if type -q eza
+   alias ls "eza -l -g --icons"
+   alias ll "ls -a"
+   alias c "clear"
+   alias nv "nvim"
+   alias t "tmux"
+   alias django-run "python manage.py runserver"
+end
+
+
+if status is-interactive
+    starship init fish | source
+    set fish_greeting
+#    if not tmux info &> /dev/null
+#       tmux attach-session -t main || tmux new-session -s main
+#    end
+
+    # Commands to run in interactive sessions can go here
+end
+
+# bun
+set -x PATH $HOME/.nix-profile/bin $PATH
+set -gx TERM xterm-256color
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
